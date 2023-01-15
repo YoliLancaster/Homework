214ddf7c1c4712265673a6af37566f1fb580cebe
YoliLancaster <yoli.lancaster@gmail.com>
2023-01-16 00:41:33 +0200

    Initial commit

---
+++ src/com/ua/robotdreams/task1.java
@@ -0,0 +1,7 @@
+package com.ua.robotdreams;
+
+public class task1 {
+    public static void main(String[] args) {
+        System.out.println("Я починаю курс Java в robot_dreams і вже вмію використовувати Git");
+    }
+}
